53400859cde56f80a47ecf305f55cb182a534d79
Hiloliddin <38894285+hiloliddinj@users.noreply.github.com>
2023-04-12 20:58:26 +0500

    score on hand worksWell

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -1,15 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private List<GameObject> _cupsInRaw;
+    [SerializeField] private TextMeshProUGUI _scoreOnHand;
 
-    private int _currentGameMoney = 0;
+    private int _currentGameMoney = 1;
 
     void Start()
     {
+        _scoreOnHand.text = _currentGameMoney.ToString();
         EventManager.current.OnCupAdded += AddCup;
         EventManager.current.OnCupCoffeeAdded += OnCupCoffeeAdded;
     }
@@ -35,8 +38,7 @@ public class GameManager : MonoBehaviour
             {
                 //EnableView
                 SwitchCup(cup, true);
-                _currentGameMoney++;
-                Debug.Log("CurrentMoney: " + _currentGameMoney);
+                UpdateHandScore(1);
                 break;
             }                
         }
@@ -62,7 +64,13 @@ public class GameManager : MonoBehaviour
 
     private void OnCupCoffeeAdded()
     {
-        _currentGameMoney++;
+        UpdateHandScore(1);
+    }
+
+    private void UpdateHandScore(int amount)
+    {
+        _currentGameMoney += amount;
         Debug.Log("CurrentMoney: " + _currentGameMoney);
+        _scoreOnHand.text = _currentGameMoney.ToString();
     }
 }
